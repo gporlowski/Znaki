c66aa67547a5ab4b28ba133525935a9edbf9b6fe
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 23:14:14 +0100

    Znaki 5

---
+++ Znaki5.java
@@ -0,0 +1,8 @@
+public class Znaki5 {
+    public static void main(String[] args) {
+        String word = "alfabet";
+        for(int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+        }
+    }
+}
