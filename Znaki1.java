d99bacdd88be768a36187c2127763476f1c89a00
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 22:32:11 +0100

    Znaki 1

---
+++ Znaki1.java
@@ -0,0 +1,9 @@
+public class Znaki1 {
+    public static void main(String[] args) {
+        char letter = 'a';
+        for(int i = 0; i <= 25; i++) {
+            System.out.print(letter + ", ");
+            letter++;
+        }
+    }
+}
