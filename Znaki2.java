29c22158e9dadd564cf77e31e5aed1150c8f45a8
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 22:35:46 +0100

    Znaki 2

---
+++ Znaki2.java
@@ -0,0 +1,9 @@
+public class Znaki2 {
+    public static void main(String[] args) {
+        char letter = 'a';
+        for(int i = 0; i <= 12; i++) {
+            System.out.print(letter + ", ");
+            letter += 2;
+        }
+    }
+}
