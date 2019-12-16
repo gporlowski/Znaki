7ade51497ca26e16808f2ab6a4b44eeed21fd3a8
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 22:37:00 +0100

    Znaki 3

---
+++ Znaki3.java
@@ -0,0 +1,9 @@
+public class Znaki3 {
+    public static void main(String[] args) {
+        char letter = 'z';
+        for(int i = 0; i <= 25; i++) {
+            System.out.print(letter + ", ");
+            letter--;
+        }
+    }
+}
